e14082ee7d8bf0e64bd22471d14829275ac4a57c
Shivam <Shivam@DESKTOP-SF553VL>
2022-06-21 10:40:23 +0530

    --M17--

---
+++ M17/src/com/firstprogram/corejava/ConcurrencyDemo.java
@@ -0,0 +1,27 @@
+package com.firstprogram.corejava;
+import java.util.concurrent.locks.*;
+public class ConcurrencyDemo {
+
+	public static void main(String[] args) {
+		ReentrantLock l=new ReentrantLock();
+		
+		l.lock();
+		System.out.println(l.isLocked());
+
+		System.out.println(l.isHeldByCurrentThread());
+		
+		System.out.println(l.getQueueLength());
+		
+		l.unlock();
+		
+		System.out.println(l.getHoldCount());
+		
+		System.out.println(l.isLocked());
+
+
+
+
+
+	}
+
+}
